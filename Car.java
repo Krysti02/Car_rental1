0b570398a98f8397780af3b9532b414b90d346c4
Krysti02 <123841750+Krysti02@users.noreply.github.com>
2023-11-30 18:23:02 +0100

    Car.java

--- Car.java
+++ Car.java
@@ -1,2 +1,43 @@
-package PACKAGE_NAME;public class Car {
+public class Car {
+    private String make;
+    private String model;
+    private double rentalPrice;
+    private int rentals;
+
+    public Car(String make, String model, double rentalPrice) {
+        this.make = make;
+        this.model = model;
+        this.rentalPrice = rentalPrice;
+        this.rentals = 0;
+    }
+
+    public String getMake() {
+        return make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public double getRentalPrice() {
+        return rentalPrice;
+    }
+
+    public int getRentals() {
+        return rentals;
+    }
+
+    public void rent() {
+        rentals++;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "make='" + make + '\'' +
+                ", model='" + model + '\'' +
+                ", rentalPrice=" + rentalPrice +
+                ", rentals=" + rentals +
+                '}';
+    }
 }
